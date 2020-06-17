1b2f92e21da345d36cfd02e1543215a7d32283f1
YeasirArafatRakib <yeasir900@gmail.com>
2020-06-17 19:14:10 +0600

    Initial commit. Created boot project and configure the application properties.

---
+++ boighor/src/main/java/com/yar/boighor/boighor/BoighorApplication.java
@@ -0,0 +1,13 @@
+package com.yar.boighor.boighor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoighorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoighorApplication.class, args);
+	}
+
+}
---
+++ boighor/src/test/java/com/yar/boighor/boighor/BoighorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yar.boighor.boighor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoighorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
