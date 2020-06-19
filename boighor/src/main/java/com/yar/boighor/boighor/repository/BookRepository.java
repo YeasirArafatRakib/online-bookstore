abde8fd21b9dc0fe0cade4337654f75c9b21b645
YeasirArafatRakib <yeasir900@gmail.com>
2020-06-19 10:51:20 +0600

    Create angular project and completed the feature 1.

--- boighor/src/main/java/com/yar/boighor/boighor/repository/BookRepository.java
+++ boighor/src/main/java/com/yar/boighor/boighor/repository/BookRepository.java
@@ -1,9 +1,10 @@
 package com.yar.boighor.boighor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.yar.boighor.boighor.entity.Book;
-
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book,Long> {
 
 }
