8868f29467f55fdac6235fe958d37dc39efdd5ab
YeasirArafatRakib <yeasir900@gmail.com>
2020-06-20 01:13:36 +0600

    Feature 2 (Frontend)And Feature 3 (Search By Category) is done with another branch.

--- angular-bookstore/src/app/app.module.ts
+++ angular-bookstore/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { BookListComponent } from './component/book-list/book-list.component';
+import { PageNotFoundComponent } from './component/page-not-found/page-not-found.component';
 import { BookService } from './services/book.service';
+
+const routes: Routes = [
+  {path:'books', component: BookListComponent},
+  {path:'category/:id', component: BookListComponent},
+  {path:'', redirectTo: '/books' , pathMatch:'full'},
+  {path:'**' , component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     BookService
